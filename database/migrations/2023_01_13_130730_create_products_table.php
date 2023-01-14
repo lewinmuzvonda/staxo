fe56fd244bd6d34e752f833b88d621fbdfbd2579
Lewin Muzvonda <muzvondalewin@gmail.com>
2023-01-14 04:29:20 +0400

    Update Product Migration

--- database/migrations/2023_01_13_130730_create_products_table.php
+++ database/migrations/2023_01_13_130730_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('image');
             $table->double('price',8,1);
             $table->integer('category');
             $table->integer('status');
