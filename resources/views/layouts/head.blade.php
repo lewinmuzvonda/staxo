b1bb424d1b6a990bb980da83fae9d4871e7960d9
Lewin Muzvonda <muzvondalewin@gmail.com>
2023-01-16 19:30:44 +0400

    Cleanup

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -19,12 +19,6 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-<link href="/assets/nav/vendor/aos/aos.css" rel="stylesheet">
-<link href="/assets/nav/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-<link href="/assets/nav/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-<link href="/assets/nav/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-<link href="/assets/nav/vendor/remixicon/remixicon.css" rel="stylesheet">
-<link href="/assets/nav/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
 @laravelViewsStyles(laravel-views,tailwindcss,livewire)
 <link href="/assets/nav/css/style.css" rel="stylesheet">
