6745a1b54e0c0b80edb7184b862ebf721fa8f0ab
Lewin Muzvonda <muzvondalewin@gmail.com>
2023-01-17 05:28:23 +0400

    After Payment Controls

--- resources/views/customer/pay.blade.php
+++ resources/views/customer/pay.blade.php
@@ -5,7 +5,7 @@
     <section class="container pt-5">
         <div class="container p-5 border-2 rounded-3 border-primary">
             <div class="row gx-4 gx-lg-5 align-items-center">
-
+                <p id="processtext" class="fw-bold text-primary"></p>
                 <form id="pay" action="{{ route('pay.post') }}" method="post">
                     @csrf
 
@@ -19,9 +19,9 @@
                     <!-- Stripe Elements Placeholder -->
                     <div id="card-element"></div>
                     
-                    <a class="btn btn-primary mt-4" onClick="hold()" id="card-button">
+                    <button class="btn btn-primary mt-4" onClick="hold()" id="card-button">
                         Process Payment
-                    </a>
+                    </button>
                 </form>
 
               
@@ -46,6 +46,8 @@
         const delay = ms => new Promise(res => setTimeout(res, ms));
 
         const hold = async () => {
+            document.getElementById('processtext').innerHTML = 'Please wait...PROCESSING';
+            cardButton.disabled = true;
             await delay(5000);
             document.getElementById("pay").submit();
         };
