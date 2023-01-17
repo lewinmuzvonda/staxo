4c53d45b32bf0448f5de636fd57f9e91f7ffda25
Lewin Muzvonda <muzvondalewin@gmail.com>
2023-01-17 14:14:37 +0400

    Confirm Delete

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.master') 
 @section('title', 'Manage Products')
+
+@push('head')
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+@endpush
+
 @section('content')
 
     <div class="container p-5">
@@ -35,16 +41,60 @@
                                     <button type="button" class="btn btn-primary">Edit</button>
                                 </a>
 
-                                <a href="{{ route('deleteproduct', $product['id']) }}">
-                                    <button id="deleteproduct" type="button" class="btn btn-danger">Delete</button>
-                                </a>
+                                <button type="button" class="btn" style="background-color: rgb(205, 19, 19);color:white" onClick="modal({{$product['id']}})" data-toggle="modal" data-target="#myModal">
+                                    Delete
+                                </button>
+                                
+                                <div class="modal fade" id="myModal{{$product['id']}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                    <div class="modal-dialog" role="document">
+                                    <div class="modal-content align-items-center">
+                                        <div class="modal-header">
+                                        <h4 class="modal-title fw-bold" id="myModalLabel">You are about to delete {{$product['name']}}</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                        Are you sure you want to continue?
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button onClick="cancel({{$product['id']}})" type="button" class="btn btn-primary" data-dismiss="modal">NO, DON'T DELETE</button>
+                                        
+                                        <a href="{{ route('deleteproduct', $product['id']) }}">
+                                            <button type="button" style="background-color: rgb(205, 19, 19);color:white" class="btn">YES, DELETE</button>
+                                        </a>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+
                             </td>
                         </tr>
                     @endforeach
                 
                 </tbody>
             </table>
+
+          
+
+
         </div>
     </div>
 
 @endsection
+@push('script')
+<script>
+
+    function modal(id){
+        $('#myModal'+id).modal('toggle');
+        $('#myModal'+id).modal('show');
+
+        $('#myModal'+id).on('shown.bs.modal', function () {
+            $('#myInput').focus()
+        });
+    }
+
+    function cancel(id){
+        $('#myModal'+id).modal('hide');
+    }
+   
+
+</script>
+@endpush
